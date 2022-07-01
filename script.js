92047deeaa3f899edbdd6096625e065505d186d7
Oyinlade Ojesanmi <Ojesanmioyinlade@gmail.com>
2022-07-01 10:52:45 +0100

    Add date

--- script.js
+++ script.js
@@ -125,3 +125,60 @@ function handleLinkClick(e) {
 document.addEventListener('click', (e) => {
   handleLinkClick(e);
 });
+
+// Date
+const timeElement = document.querySelector('.time');
+const dateElement = document.querySelector('.date');
+
+/**
+ * @param {Date} date
+ */
+function formatTime(date) {
+  const hours12 = date.getHours() % 12 || 12;
+  const minutes = date.getMinutes();
+  const isAm = date.getHours() < 12;
+
+  return `${hours12.toString().padStart(2, '0')}:${minutes
+    .toString()
+    .padStart(2, '0')} ${isAm ? 'AM' : 'PM'}`;
+}
+
+/**
+ * @param {Date} date
+ */
+function formatDate(date) {
+  const DAYS = [
+    'Sunday',
+    'Monday',
+    'Tuesday',
+    'Wednesday',
+    'Thursday',
+    'Friday',
+    'Saturday',
+  ];
+  const MONTHS = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December',
+  ];
+
+  return `${DAYS[date.getDay()]}, ${
+    MONTHS[date.getMonth()]
+  } ${date.getDate()} ${date.getFullYear()}`;
+}
+
+setInterval(() => {
+  const now = new Date();
+
+  timeElement.textContent = formatTime(now);
+  dateElement.textContent = formatDate(now);
+}, 200);
